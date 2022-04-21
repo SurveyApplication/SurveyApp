9b9febe39f3faa3b9b689b06f065aa15e50f5836
SurveyApplication <104172840+SurveyApplication@users.noreply.github.com>
2022-04-21 16:27:49 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports.SEND_MAIL_CONFIG = {
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    auth: {
+        user: 'surveyapplication2022@gmail.com',
+        pass: 'AdminHSB2022',
+    },
+};
+
